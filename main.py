cdc62015efd6380baac8e032cff33c93bb02e722
Alex Ciobanu <alex.ciobanu.a@gmail.com>
2017-12-11 17:11:49 +0200

    Menubar Addded

--- main.py
+++ main.py
@@ -8,9 +8,29 @@ class CenteredFrame(wx.Frame):
         self.Show()
 
 
+class MenuFrame(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(MenuFrame, self).__init__(*args, **kwargs)
+        self.InitUi()
+
+    def InitUi(self):
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        file_menu_exit = file_menu.Append(wx.ID_EXIT, 'Eject', 'Ankide acola')
+        menu_bar.Append(file_menu, '&File')
+        self.SetMenuBar(menu_bar)
+        self.Bind(wx.EVT_MENU, self.OnQuit, file_menu_exit)
+        self.SetSize((300, 200))
+        self.SetTitle('Frame with menu')
+        self.Center()
+        self.Show()
+
+    def OnQuit(self, e):
+        self.Close()
+
+
 if __name__ == '__main__':
     print("Here it goes")
     app = wx.App()
-    CenteredFrame(None, "Op Germania")
-
+    MenuFrame(None)
     app.MainLoop()
