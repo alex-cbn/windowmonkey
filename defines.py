ebe4f38887283b7efb16a1b7041a29e4d7a3ef5f
Alex Ciobanu <alex.ciobanu.a@gmail.com>
2017-12-19 06:10:43 +0200

    actual files to be changed added

---
+++ defines.py
@@ -0,0 +1,9 @@
+#SEND EMAIL SUBJECTS
+SEND_MAIL_SUBIECT_SUCCESS   = "Email succes!"
+SEND_MAIL_BODY_SUCCESS      = "Tema a fost inregistrata cu succes!"
+SEND_MAIL_SUBIECT_ERROR     = "Email error!"
+
+# ERROR MESSAGES
+ERROR_BODY_SUBIECT_INCORECT    = "Subiectul emailului este incorect!"
+ERROR_BODY_ARHIVA_INEXISTENTA  = "Nu exista o arhiva atasata emailului!"
+ERROR_BODY_ARHIVA_INCORECTA    = "Arhiva nu este denumita corespunzator!"
